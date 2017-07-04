50c33579350bb90f09ad685237c34b15e963d70d
user <priyanka.vantage123@gmail.com>
2017-07-04 19:15:43 +0530

    updates

---
+++ www/js/config.js
@@ -0,0 +1,8 @@
+
+var krms_config ={		
+	'ApiUrl':"http://www.foodrequest.arabmobilecontent.com/mobileapp/api",				
+	'DialogDefaultTitle':"AMC",	
+	'pushNotificationSenderid':"9993644539",	
+	'facebookAppId':"Y1469371919773263", 
+	'APIHasKey':"6ba1b75db3f5491671ef9e732e400b3b"
+};
